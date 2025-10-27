7cc0adce98f4af358380c7db550ece6d2733ae92
K.V.Surendra <96002022+Surendra2003@users.noreply.github.com>
2025-10-27 21:46:47 +0530

    Update assets.js

--- admin/src/assets/assets.js
+++ admin/src/assets/assets.js
@@ -14,4 +14,4 @@ export const assets ={
     parcel_icon
 }
 
-export const url = 'https://food-delivery-application-bakend.onrender.com'
+export const url = 'https://food-delivery-application-backend.onrender.com'
