ca648a617bc1df34841ac1543bcb356a5e104051
K.V.Surendra <96002022+Surendra2003@users.noreply.github.com>
2025-10-27 19:50:43 +0530

    Update assets.js
    Add render URl

--- admin/src/assets/assets.js
+++ admin/src/assets/assets.js
@@ -14,4 +14,4 @@ export const assets ={
     parcel_icon
 }
 
-export const url = 'https://food-delivery-backend-u2ol.onrender.com'
+export const url = 'https://food-delivery-application-bakend.onrender.com'
