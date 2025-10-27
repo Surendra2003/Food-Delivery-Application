27a36076e28ad1fb1d4f77ac8d51aea644bf23ad
K.V.Surendra <96002022+Surendra2003@users.noreply.github.com>
2025-10-27 20:47:05 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,6 +1,11 @@
 import mongoose from "mongoose";
 
 
-export const connectDB = async () =>{
-    await mongoose.connect('mongodb+srv://KOYALAKUNTLA:7032810622@cluster0.rc29euw.mongodb.net/FoodDeliveryApp').then(()=>console.log("Database connected..!"));
-}
+// export const connectDB = async () =>{
+//     await mongoose.connect('mongodb+srv://KOYALAKUNTLA:7032810622@cluster0.rc29euw.mongodb.net/FoodDeliveryApp').then(()=>console.log("Database connected..!"));
+// }
+
+
+await mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("Database connected..!"))
+  .catch((err) => console.error("DB Connection failed:", err));
