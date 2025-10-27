5e0c6561583e86dfe0aaa4833a69e47051246aca
K.V.Surendra <96002022+Surendra2003@users.noreply.github.com>
2025-10-27 22:50:54 +0530

    Update App.jsx

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -10,7 +10,8 @@ import 'react-toastify/dist/ReactToastify.css';
 
 const App = () => {
 
-  const url = "http://localhost:4000"
+  // const url = "http://localhost:4000"
+  const url = "https://food-delivery-application-backend-6l9d.onrender.com"
 
 
 
